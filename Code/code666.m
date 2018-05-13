922a030c42db9db0a207113d5a73615963be730a
miftanurfarid <miftanurfarid@gmail.com>
2018-05-13 12:34:42 +0700

    add frequency max

--- Code/code666.m
+++ Code/code666.m
@@ -1,4 +1,4 @@
-clear all; close all; clc;
+clearvars; close all; clc;
 addpath(genpath(pwd));
 
 % check if using windows or linux
@@ -39,6 +39,11 @@ l = length(data1);
 nfft = 2^nextpow2(l);
 f = fs/2*linspace(0,1,nfft/2+1);
 
+% frekuensi tertinggi yang ingin ditampilkan
+fmax = 475;
+[fval, fpos] = min((f - fmax) .^ 2); % mencari frequency bin 475 hz
+
+
 DATA1(:,1) = fft(data1(:,2),nfft)/l;
 DATA1(:,2) = fft(data1(:,3),nfft)/l;
 DATA1(:,3) = fft(data1(:,4),nfft)/l;
@@ -76,23 +81,23 @@ C3 = imag(20*log10(2*imag(DATA3(1:nfft/2+1,3))));
 D3 = imag(20*log10(2*imag(DATA3(1:nfft/2+1,4))));
 E3 = imag(20*log10(2*imag(DATA3(1:nfft/2+1,5))));
 
-[alpha11,par11]=rfp(A1(1:3841),f(1:3841)',5);
-[alpha12,par12]=rfp(B1(1:3841),f(1:3841)',5);
-[alpha13,par13]=rfp(C1(1:3841),f(1:3841)',5);
-[alpha14,par14]=rfp(D1(1:3841),f(1:3841)',5);
-[alpha15,par15]=rfp(E1(1:3841),f(1:3841)',5);
+[alpha11,par11]=rfp(A1(1:fpos),f(1:fpos)',5);
+[alpha12,par12]=rfp(A1(1:fpos),f(1:fpos)',5);
+[alpha13,par13]=rfp(C1(1:fpos),f(1:fpos)',5);
+[alpha14,par14]=rfp(D1(1:fpos),f(1:fpos)',5);
+[alpha15,par15]=rfp(E1(1:fpos),f(1:fpos)',5);
 
-[alpha21,par21]=rfp(A2(1:3841),f(1:3841)',5);
-[alpha22,par22]=rfp(B2(1:3841),f(1:3841)',5);
-[alpha23,par23]=rfp(C2(1:3841),f(1:3841)',5);
-[alpha24,par24]=rfp(D2(1:3841),f(1:3841)',5);
-[alpha25,par25]=rfp(E2(1:3841),f(1:3841)',5);
+[alpha21,par21]=rfp(A2(1:fpos),f(1:fpos)',5);
+[alpha22,par22]=rfp(B2(1:fpos),f(1:fpos)',5);
+[alpha23,par23]=rfp(C2(1:fpos),f(1:fpos)',5);
+[alpha24,par24]=rfp(D2(1:fpos),f(1:fpos)',5);
+[alpha25,par25]=rfp(E2(1:fpos),f(1:fpos)',5);
 
-[alpha31,par31]=rfp(A3(1:3841),f(1:3841)',5);
-[alpha32,par32]=rfp(B3(1:3841),f(1:3841)',5);
-[alpha33,par33]=rfp(C3(1:3841),f(1:3841)',5);
-[alpha34,par34]=rfp(D3(1:3841),f(1:3841)',5);
-[alpha35,par35]=rfp(E3(1:3841),f(1:3841)',5);
+[alpha31,par31]=rfp(A3(1:fpos),f(1:fpos)',5);
+[alpha32,par32]=rfp(B3(1:fpos),f(1:fpos)',5);
+[alpha33,par33]=rfp(C3(1:fpos),f(1:fpos)',5);
+[alpha34,par34]=rfp(D3(1:fpos),f(1:fpos)',5);
+[alpha35,par35]=rfp(E3(1:fpos),f(1:fpos)',5);
 
 alpha11=abs(alpha11)';
 alpha12=abs(alpha12)';
@@ -115,11 +120,11 @@ alpha35=abs(alpha35)';
 %% plot
 % rfp bola
 figure;
-plot(f(1:3841),log10(alpha11)); xlim([0 750]); hold on;
-plot(f(1:3841),log10(alpha12)); xlim([0 750]);
-plot(f(1:3841),log10(alpha13)); xlim([0 750]);
-plot(f(1:3841),log10(alpha14)); xlim([0 750]);
-plot(f(1:3841),log10(alpha15)); xlim([0 750]); axis tight; hold off;
+plot(f(1:fpos),log10(alpha11));hold on;
+plot(f(1:3841),log10(alpha12));
+plot(f(1:fpos),log10(alpha13));
+plot(f(1:fpos),log10(alpha14));
+plot(f(1:fpos),log10(alpha15));axis tight; hold off;
 title('Frequency Response Function(bola)');
 ylabel('Amplitude(m/s^2)');
 xlabel('Frequency (Hz)');
@@ -127,11 +132,11 @@ legend('Node 1','Node 2','Node 3','Node 4','Node 5','location','northwest');
 
 % rfp dorong
 figure;
-plot(f(1:3841),log10(alpha21)); xlim([0 750]); hold on;
-plot(f(1:3841),log10(alpha22)); xlim([0 750]);
-plot(f(1:3841),log10(alpha23)); xlim([0 750]);
-plot(f(1:3841),log10(alpha24)); xlim([0 750]);
-plot(f(1:3841),log10(alpha25)); xlim([0 750]); axis tight; hold off;
+plot(f(1:fpos),log10(alpha21));hold on;
+plot(f(1:fpos),log10(alpha22));
+plot(f(1:fpos),log10(alpha23));
+plot(f(1:fpos),log10(alpha24));
+plot(f(1:fpos),log10(alpha25));axis tight; hold off;
 title('Frequency Response Function (dorong)');
 ylabel('Amplitude(m/s^2)');
 xlabel('Frequency (Hz)');
@@ -139,11 +144,11 @@ legend('Node 1','Node 2','Node 3','Node 4','Node 5','location','northwest');
 
 % rfp lompat
 figure;
-plot(f(1:3841),log10(alpha31)); xlim([0 750]); hold on;
-plot(f(1:3841),log10(alpha32)); xlim([0 750]);
-plot(f(1:3841),log10(alpha33)); xlim([0 750]);
-plot(f(1:3841),log10(alpha34)); xlim([0 750]);
-plot(f(1:3841),log10(alpha35)); xlim([0 750]); axis tight; hold off;
+plot(f(1:fpos),log10(alpha31));hold on;
+plot(f(1:fpos),log10(alpha32));
+plot(f(1:fpos),log10(alpha33));
+plot(f(1:fpos),log10(alpha34));
+plot(f(1:fpos),log10(alpha35));axis tight; hold off;
 title('Frequency Response Function (loncat)');
 ylabel('Amplitude(m/s^2)');
 xlabel('Frequency (Hz)');
@@ -151,45 +156,45 @@ legend('Node 1','Node 2','Node 3','Node 4','Node 5','location','northwest');
 
 % rfp bola + dorong + lompat
 figure;
-plot(f(1:3841),log10(alpha11)); xlim([0 750]); hold on;
-plot(f(1:3841),log10(alpha21)); xlim([0 750]);
-plot(f(1:3841),log10(alpha31)); xlim([0 750]); axis tight; hold off;
+plot(f(1:fpos),log10(alpha11));hold on;
+plot(f(1:fpos),log10(alpha21));
+plot(f(1:fpos),log10(alpha31));axis tight; hold off;
 title('Frequency Response Function (Node 1)');
 ylabel('Amplitude(m/s^2)');
 xlabel('Frequency (Hz)');
 legend('Bola', 'Lompat', 'Dorong', 'location', 'northwest');
 
 figure;
-plot(f(1:3841),log10(alpha12)); xlim([0 750]); hold on;
-plot(f(1:3841),log10(alpha22)); xlim([0 750]);
-plot(f(1:3841),log10(alpha32)); xlim([0 750]); axis tight; hold off;
+plot(f(1:fpos),log10(alpha12));hold on;
+plot(f(1:fpos),log10(alpha22));
+plot(f(1:fpos),log10(alpha32));axis tight; hold off;
 title('Frequency Response Function (Node 2)');
 ylabel('Amplitude(m/s^2)');
 xlabel('Frequency (Hz)');
 legend('Bola', 'Lompat', 'Dorong', 'location', 'northwest');
 
 figure;
-plot(f(1:3841),log10(alpha13)); xlim([0 750]); hold on;
-plot(f(1:3841),log10(alpha23)); xlim([0 750]);
-plot(f(1:3841),log10(alpha33)); xlim([0 750]); axis tight; hold off;
+plot(f(1:fpos),log10(alpha13));hold on;
+plot(f(1:fpos),log10(alpha23));
+plot(f(1:fpos),log10(alpha33));axis tight; hold off;
 title('Frequency Response Function (Node 3)');
 ylabel('Amplitude(m/s^2)');
 xlabel('Frequency (Hz)');
 legend('Bola', 'Lompat', 'Dorong', 'location', 'northwest');
 
 figure;
-plot(f(1:3841),log10(alpha14)); xlim([0 750]); hold on;
-plot(f(1:3841),log10(alpha24)); xlim([0 750]);
-plot(f(1:3841),log10(alpha34)); xlim([0 750]); axis tight; hold off;
+plot(f(1:fpos),log10(alpha14));hold on;
+plot(f(1:fpos),log10(alpha24));
+plot(f(1:fpos),log10(alpha34));axis tight; hold off;
 title('Frequency Response Function (Node 4)');
 ylabel('Amplitude(m/s^2)');
 xlabel('Frequency (Hz)');
 legend('Bola', 'Lompat', 'Dorong', 'location', 'northwest');
 
 figure;
-plot(f(1:3841),log10(alpha15)); xlim([0 750]); hold on;
-plot(f(1:3841),log10(alpha25)); xlim([0 750]);
-plot(f(1:3841),log10(alpha35)); xlim([0 750]); axis tight; hold off;
+plot(f(1:fpos),log10(alpha15));hold on;
+plot(f(1:fpos),log10(alpha25));
+plot(f(1:fpos),log10(alpha35));axis tight; hold off;
 title('Frequency Response Function (Node 5)');
 ylabel('Amplitude(m/s^2)');
 xlabel('Frequency (Hz)');
